37ea493e3ccdfa8fc6afdf7eac01853861bb14e5
Nhi0612 <66456615+Nhi0612@users.noreply.github.com>
2020-11-01 18:42:01 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+
+define('DB_SERVER', 'ec2-3-208-224-152.compute-1.amazonaws.com');
+define('DB_USERNAME', 'lvwkagdnejiggw');
+define('DB_PASSWORD', 'e2292f87501fc7d98ccbee1b64b347da9cfc0e942e1c6416ce3c0be2d135d565');
+define('DB_NAME', 'denairoprqbebh');
+ 
+/* Attempt to connect to PostgreSQL database */
+$link = pg_connect("host=".DB_SERVER." dbname=". DB_NAME ." user=" . DB_USERNAME . " password=" .DB_PASSWORD. "")
+		or die('Could not connect1: ' . pg_last_error());
+?>
+
